b87b09bfc34b68661b0b618bb564d5c28cc02c86
codenigma1 <noobstep.8@gmail.com>
2025-07-20 13:37:31 +1000

    Adding staging slot

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>🚀 Hello from Flask App deployed via GitHub on Azure!</h1>'
+    return '<h1>🚀 Hello from Flask App deployed via GitHub on Azure but on staging slot!</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
